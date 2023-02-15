c1e57279ebf10a576f56027ecff327eff57d68f5
Александр Бубнов <115075731+GCranK@users.noreply.github.com>
2023-02-15 23:37:40 +0500

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,106 @@
+const textTranslations = {
+    "home": {
+        en: 'Home',
+        ru: 'Главная'
+    },
+    "about_me": {
+        en: 'About me',
+        ru: 'Обо мне'
+    },
+    "skills": {
+        en: 'Skills',
+        ru: 'Навыки'
+    },
+    "portfolio": {
+        en: 'Portfolio',
+        ru: 'Портфолио'
+    },
+    "contacts": {
+        en: 'Contacts',
+        ru: 'Контакты'
+    },
+    "name": {
+        en: 'Denis',
+        ru: 'Денис',
+    },
+    "surname": {
+        en: 'Novik',
+        ru: 'Новик',
+    },
+    "profession": {
+        en: 'UX | UI designer',
+        ru: 'UX | UI дизайнер'
+    },
+    "location_info": {
+        en: '24 years old, Minsk',
+        ru: '24 года, Минск'
+    },
+    "about_me_title": {
+        en: 'About me',
+        ru: 'Обо мне'
+    },
+    "about_text1": {
+        en: `Hi, I'm Denis – UX/UI designer from Minsk.`,
+        ru: 'Здравствуйте, меня зовут Denis – UX/UI дизайнер из Минска.'
+    },
+    "about_text2": {
+        en: `I m interested in design and everything connected with it.`,
+        ru: 'Я заинтересован в дизайне и всем, что с ним связано.'
+    },
+    "about_text3": {
+        en: `I m studying at courses "Web and mobile design`,
+        ru: 'Я учусь на курсах "Web и мобильный дизайн'
+    },
+    "about_text4": {
+        en: `interfaces" in IT-Academy.`,
+        ru: 'интерфейсов" в IT-Академии.',
+    },
+    "about_text5": {
+        en: 'Ready to implement excellent projects',
+        ru: 'Готов реализовать отличные проекты '
+    },
+    "about_text6": {
+        en: 'with wonderful people.',
+        ru: 'с замечательными людьми.'
+    },
+    "skills_title": {
+        en: 'Skills',
+        ru: 'Навыки'
+    },
+    "skills_text": {
+        en: 'I work in such programs as',
+        ru: 'Я работаю в таких программах как'
+    },
+    "portfolio_title": {
+        en: 'Portfolio',
+        ru: 'Портфолио'
+    },
+    "portfolio_text1": {
+        en: 'Online fashion store - Homepage',
+        ru: 'Интернет-магазин модной одежды - Главная страница'
+    },
+    "portfolio_text2": {
+        en: 'Reebok Store - Concept',
+        ru: 'Магазин Reebok - Концепция'
+    },
+    "portfolio_text3": {
+        en: 'Braun Landing Page - Concept',
+        ru: 'Landing страница Braun - Концепция'
+    },
+    "footer_contacts": {
+        en: 'Contacts',
+        ru: 'Контакты'
+    },
+    "footer_text1": {
+        en: 'Want to know more or just chat?',
+        ru: 'Хотите узнать больше или просто поболтать?'
+    },
+    "footer_text2": {
+        en: 'You are welcome!',
+        ru: 'Добро пожаловать!'
+    },
+    "message": {
+        en: 'Send message',
+        ru: 'Отправить сообщение'
+    },
+}
